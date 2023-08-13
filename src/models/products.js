0bd8b5c75a34e020871dab1a9bfb5a75dcfc9afc
Lado Asambadze <lado.asambadze1@gmail.com>
2023-08-13 16:34:12 +0400

    upload: photos for test front

--- src/models/products.js
+++ src/models/products.js
@@ -1,37 +1,39 @@
 import { model, Schema } from "mongoose";
 
 const products = new Schema({
-  model: {
-    clothes: {
-      type: {
-        type: Schema.Types.String,
-      },
-      gender: {
-        type: Schema.Types.String,
-      },
-      category: {
-        type: Schema.Types.String,
-      },
-      price: {
-        type: Schema.Types.Number,
-      },
-      size: {
-        type: Schema.Types.String,
-      },
-      brand: {
-        type: Schema.Types.String,
-      },
-      name: {
-        type: Schema.Types.String,
-      },
-      image: {
-        type: Schema.Types.Array,
-      },
-      new: {
-        type: Schema.Types.Boolean,
-      },
-    },
+  type: {
+    type: Schema.Types.String,
   },
+  gender: {
+    type: Schema.Types.String,
+  },
+  category: {
+    type: Schema.Types.String,
+  },
+  price: {
+    type: Schema.Types.Number,
+  },
+  size: {
+    type: [Schema.Types.Number],
+  },
+  brand: {
+    type: Schema.Types.String,
+  },
+  name: {
+    type: Schema.Types.String,
+  },
+  images: [
+    {
+      color: {
+        type: Schema.Types.String,
+      },
+      urls: [
+        {
+          type: Schema.Types.String,
+        },
+      ],
+    },
+  ],
 });
 
 export const productsData = model("products", products);
